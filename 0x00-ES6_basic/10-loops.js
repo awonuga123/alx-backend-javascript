dc82fc6d3e78755242515d0f0e74658fdffc82e7
awonuga123 <awonugaismail132@gmail.com>
2023-03-20 13:19:05 -0700

    welcome to ES6

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+  for (let idx of array) {
+    let value = `${appendString}i
+  }
+
+  return value;
+}
---
+++ 0x00-ES6_basic/README.md
@@ -0,0 +1,1 @@
+This is a basic introduction to ES6 (ECMAScript 2015) features, which is the latest version of the ECMAScript standard that defines the JavaScript language.
